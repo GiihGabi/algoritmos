695bc1bdd476fa8f33154d19a80ef94fa2b5d204
unknown <giovannacostabatista@gmail.com>
2022-12-05 13:33:57 -0300

    [200~Inserindo o código de teste de idade

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # algoritmos
 Nesse repositório terão vários trabalhos e desafios em pyhton.
+
+# 1°EXERCÍCIO - MAIOR DE IDADE
---
+++ idade.py
@@ -0,0 +1,7 @@
+idade = int (input("informe sua idade : "))
+
+#se idade > 18 entao:
+if idade >= 18: 
+  print("Você é maior de idade")
+else:
+  print("Você é menor de idade")
